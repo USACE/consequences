ed3307bb00fac40bf7ca9aefbc9b8bcd85c16214
Jack <jackmschonherr@gmail.com>
2024-08-13 15:55:29 -0700

    Implemented ResultItem generic class #10

---
+++ Consequences/ResultItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consequences;
+public class ResultItem<T>
+{
+  private string resultName;
+  private T result;
+
+  public string Name()
+  {
+    return resultName;
+  }
+  public T Value() 
+  {
+    return result;
+  }
+
+}
