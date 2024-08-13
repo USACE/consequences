089d7b165462022df0fdc3d8516b6332eea46cdd
Jack <jackmschonherr@gmail.com>
2024-08-13 15:37:28 -0700

    Implemented the HazardParameter interface #9

---
+++ Consequences/IHazardInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consequences;
+internal interface IHazardInterface
+{
+  internal bool Has(HazardParameter hp);
+  internal T Get<T>(HazardParameter hp);
+}
