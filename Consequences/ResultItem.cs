773b62a673437997943cf81493441ddea07aa709
Jack <jackmschonherr@gmail.com>
2024-08-14 09:36:30 -0700

    Removed lines auto filled by visual studio

--- Consequences/ResultItem.cs
+++ Consequences/ResultItem.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Consequences;
+﻿namespace Consequences;
 public class ResultItem<T>
 {
   private string resultName;
   private T result;
 
   public string Name()
-  {
+  { 
     return resultName;
   }
   public T Value() 
   {
     return result;
   }
-
 }
