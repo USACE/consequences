82d09ab855d48a321c598df85e0ce093403ffb49
Jack <jackmschonherr@gmail.com>
2024-08-14 09:50:22 -0700

    added constructor to ResultItem class #10

--- Consequences/ResultItem.cs
+++ Consequences/ResultItem.cs
@@ -4,6 +4,12 @@ public class ResultItem<T>
   private string resultName;
   private T result;
 
+  public ResultItem(string name, T res)
+  {
+    resultName = name;
+    result = res;
+  }
+
   public string Name()
   { 
     return resultName;
