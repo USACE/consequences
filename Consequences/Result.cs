0e3b91c03e99888a90f299ce7eb9134a9923d69e
Jack <jackmschonherr@gmail.com>
2024-08-14 10:05:57 -0700

    implemented a fetch method for ResultItem #11

--- Consequences/Result.cs
+++ Consequences/Result.cs
@@ -2,4 +2,16 @@
 public struct Result
 {
   private ResultItem<object>[] results;
+
+  public ResultItem<object> Fetch(string name)
+  {
+    for (int i = 0; i < results.Length; i++)
+    {
+      if (results[i].Name() == name) 
+      { 
+        return results[i];
+      }
+    }
+    return null;
+  }
 }
