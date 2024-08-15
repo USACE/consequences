01075d010303ee8e494e333b348c8be83a073788
Jack <jackmschonherr@gmail.com>
2024-08-15 13:40:15 -0700

    added constructor and changed return if name not found

--- Consequences/Results/Result.cs
+++ Consequences/Results/Result.cs
@@ -1,14 +1,22 @@
 ﻿namespace USACE.HEC.Results;
 public class Result
 {
-  private ResultItem[] results;
+  private ResultItem[] _results;
+
+  public Result(ResultItem[] results)
+  { 
+    _results = results;
+  }
 
   // retrieve a ResultItem from a Result by name
   public ResultItem Fetch(string name)
   {
-    for (int i = 0; i < results.Length; i++)
-      if (results[i].ResultName == name)
-        return results[i];
-    throw new Exception("Name not found.");
+    for (int i = 0; i < _results.Length; i++)
+      if (_results[i].ResultName == name)
+        return _results[i];
+    // return empty ResultItem if not found
+    ResultItem item = new ResultItem();
+    item.ResultName = name;
+    return item;
   }
 }
