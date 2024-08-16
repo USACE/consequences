8174301d0602f9cad583502517ae313fbba69c5e
Jack <jackmschonherr@gmail.com>
2024-08-16 08:25:31 -0700

    renaming in Result, added unit test file

--- Consequences/Results/Result.cs
+++ Consequences/Results/Result.cs
@@ -1,19 +1,19 @@
 ﻿namespace USACE.HEC.Results;
 public class Result
 {
-  private ResultItem[] _results;
+  private ResultItem[] _resultItems;
 
-  public Result(ResultItem[] results)
+  public Result(ResultItem[] resultItems)
   { 
-    _results = results;
+    _resultItems = resultItems;
   }
 
   // retrieve a ResultItem from a Result by name
   public ResultItem Fetch(string name)
   {
-    for (int i = 0; i < _results.Length; i++)
-      if (_results[i].ResultName == name)
-        return _results[i];
+    for (int i = 0; i < _resultItems.Length; i++)
+      if (_resultItems[i].ResultName == name)
+        return _resultItems[i];
     // return empty ResultItem if not found
     ResultItem item = new ResultItem();
     item.ResultName = name;
---
+++ ConsequencesTest/DepthHazardTest.cs
@@ -0,0 +1,15 @@
+﻿using USACE.HEC.Hazards;
+
+namespace ConsequencesTest;
+public class DepthHazardTest
+{
+  [Fact]
+  public void Test() 
+  {
+    DepthHazard dh = new DepthHazard(1.01f);
+    bool has = dh.Has(HazardParameter.Depth);
+    float depth = dh.Get<float>(HazardParameter.Depth);
+
+
+  }
+}
