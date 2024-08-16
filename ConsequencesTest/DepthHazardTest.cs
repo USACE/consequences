a035bb945934f15e56d8081f3e8787fb7309f4ca
Jack <jackmschonherr@gmail.com>
2024-08-16 09:34:52 -0700

    added parameter checking to Get

--- ConsequencesTest/DepthHazardTest.cs
+++ ConsequencesTest/DepthHazardTest.cs
@@ -1,15 +1,31 @@
 ﻿using USACE.HEC.Hazards;
+using Xunit.Sdk;
 
 namespace ConsequencesTest;
 public class DepthHazardTest
 {
   [Fact]
-  public void Test() 
+  public void TestInterfaceImplementation() 
   {
-    DepthHazard dh = new DepthHazard(1.01f);
+    float input = 1.01f;
+    IHazard dh = new DepthHazard(input);
     bool has = dh.Has(HazardParameter.Depth);
-    float depth = dh.Get<float>(HazardParameter.Depth);
+    if (has)
+    {
+      float depth = dh.Get<float>(HazardParameter.Depth);
+      Assert.Equal(input, depth);
+    } else
+    {
+      throw new Exception("Failed to find appropriate parameter");
+    }
 
+  }
+
+  [Fact]
+  public void TestGetWrongParameter()
+  {
+    IHazard dh = new DepthHazard(1.01f);
+    Assert.Throws<NotSupportedException>(() => dh.Get<float>(HazardParameter.ArrivalTime));
 
   }
 }
