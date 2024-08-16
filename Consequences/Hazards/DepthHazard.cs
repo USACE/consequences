f95a3b1c90c8a92788e7577ef924970f56aff3e3
Jack <jackmschonherr@gmail.com>
2024-08-16 08:25:13 -0700

    initial DepthHazard

---
+++ Consequences/Hazards/DepthHazard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USACE.HEC.Hazards;
+public class DepthHazard : IHazard
+{
+  private float _depth;
+  public DepthHazard(float depth)
+  {
+    _depth = depth;
+  }
+
+  public bool Has(HazardParameter hp)
+  {
+    return (hp & HazardParameter.Depth) == hp;
+  }
+
+  public T Get<T>(HazardParameter hp)
+  {
+    // return ((T?)_depth).GetValueOrDefault();
+
+    if (typeof(T) == typeof(float))
+    {
+      return (T)(object)_depth;
+    }
+
+    throw new NotSupportedException();
+  }
+}
