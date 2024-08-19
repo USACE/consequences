9dadb5079de944a17bd8ac48c3c5b695c45414ca
Jack <jackmschonherr@gmail.com>
2024-08-19 11:18:11 -0700

    implemented IResultsWriter #16

---
+++ Consequences/Results/IResultsWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USACE.HEC.Results;
+public interface IResultsWriter : IDisposable
+{
+  public void Write(Result res);
+}
