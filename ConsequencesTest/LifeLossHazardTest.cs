b731513f99fa25bceb8f368b225cf88320340d90
Jack <jackmschonherr@gmail.com>
2024-08-19 10:26:35 -0700

    implemented get for LifeLossHazard, added tests #15

--- ConsequencesTest/LifeLossHazardTest.cs
+++ ConsequencesTest/LifeLossHazardTest.cs
@@ -4,7 +4,10 @@ using Xunit.Sdk;
 namespace ConsequencesTest;
 public class LifeLossHazardTest
 {
-  IHazard llh = new LifeLossHazard(1.01f, 1.01f, DateTime.Now);
+  static float depth = 1.01f;
+  static float velocity = 6.03f;
+  static DateTime time = new DateTime(2024, 8, 10);
+  IHazard llh = new LifeLossHazard(depth, velocity, time);
 
   [Fact]
   public void TestHasCorrectParameter()
@@ -22,6 +25,36 @@ public class LifeLossHazardTest
     Assert.False(llh.Has(HazardParameter.ArrivalTime));
     // compound parameter, must have all individual parameters to pass
     Assert.False(llh.Has(HazardParameter.ArrivalTime | HazardParameter.Depth));
+  }
+
+  [Fact]
+  public void TestGetCorrectValue()
+  {
+    Assert.Equal(depth, llh.Get<float>(HazardParameter.Depth));
+    Assert.Equal(velocity, llh.Get<float>(HazardParameter.Velocity));
+    Assert.Equal(time, llh.Get<DateTime>(HazardParameter.ArrivalTime2ft));
+  }
+
+  [Fact]
+  public void TestGetIncorrectValue()
+  {
+    Assert.NotEqual(depth + 0.1f, llh.Get<float>(HazardParameter.Depth));
+    Assert.NotEqual(velocity + 0.1f, llh.Get<float>(HazardParameter.Velocity));
+    Assert.NotEqual(DateTime.Now, llh.Get<DateTime>(HazardParameter.ArrivalTime2ft));
+  }
+
+  [Fact]
+  public void TestGetInvalidParameter()
+  {
+    Assert.Throws<NotSupportedException>(() => llh.Get<float>(HazardParameter.ArrivalTime));
+    Assert.Throws<NotSupportedException>(() => llh.Get<float>(HazardParameter.Velocity | HazardParameter.Depth));
+  }
+
+  [Fact]
+  public void TestGetInvalidType()
+  {
+    Assert.Throws<InvalidCastException>(() => llh.Get<int>(HazardParameter.Depth));
+    Assert.Throws<InvalidCastException>(() => llh.Get<string>(HazardParameter.Velocity));
   }
 }
 
