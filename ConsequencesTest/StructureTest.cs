218f13b00bfd525ec7457ff0f5430dcda7779dff
Jack <jackmschonherr@gmail.com>
2024-08-19 16:23:01 -0700

    Implemented functionality and tests for first unit test of Structure #18

---
+++ ConsequencesTest/StructureTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USACE.HEC.Results;
+using USACE.HEC.Consequences;
+using USACE.HEC.Hazards;
+
+namespace ConsequencesTest;
+public class StructureTest
+{
+  [Fact]
+  public void TestSimpleDepth()
+  {
+    Structure s = new Structure();
+    IHazard dh = new DepthHazard(4.56f);
+    Result res = s.Compute(dh);
+    ResultItem item1 = res.Fetch("Depth");
+
+    Assert.Equal("Depth", item1.ResultName);
+    Assert.Equal(typeof(float), item1.Result.GetType());
+    Assert.Equal(4.56f, item1.Result);
+  }
+}
