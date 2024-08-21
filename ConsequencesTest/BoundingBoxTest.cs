b29ab84d1b01198dfcc231496a0f73fafa591573
Jack <jackmschonherr@gmail.com>
2024-08-21 11:28:12 -0700

    implemented Location and BoundingBox, not sure about GDAL format for BB yet though #19

---
+++ ConsequencesTest/BoundingBoxTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USACE.HEC.Geography;
+
+namespace ConsequencesTest;
+public class BoundingBoxTest
+{
+  [Fact]
+  public void TestNSI()
+  {
+    Location upperLeft = new Location { X = -40, Y = 50 };
+    Location lowerRight = new Location { X = 60, Y = -50 };
+    BoundingBox boundingBox = new BoundingBox(upperLeft, lowerRight);
+
+    string NSIFormat = boundingBox.NSIFormat();
+
+    string expected = "-40,50,60,50,60,-50,-40,-50,-40,50";
+    Assert.Equal(expected, NSIFormat);
+  }
+}
