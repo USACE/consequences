b8bcdd5b3665f7866191fb9580c14ef986318e6e
Jack <jackmschonherr@gmail.com>
2024-08-22 14:48:54 -0700

    added initial interfaces and implementations for processors after the video call #23 #24 #25

---
+++ Consequences/Consequences/NSIStreamingProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USACE.HEC.Geography;
+
+namespace USACE.HEC.Consequences;
+public class NSIStreamingProcessor : IBBoxStreamingProcessor
+{
+  public void Process(BoundingBox boundingBox, Action<IConsequencesReceptor> ConsequenceReceptorProcess)
+  {
+    Structure s = new Structure();
+    ConsequenceReceptorProcess(s);
+  }
+
+
+
+  /*
+  static async Task Test()
+  {
+    // Define the API endpoint
+    string apiEndpoint = "https://nsi.sec.usace.army.mil/nsiapi/structures?bbox=-81.58418,30.25165,-81.58161,30.26939,-81.55898,30.26939,-81.55281,30.24998,-81.58418,30.25165";
+
+    // Create an instance of HttpClient
+    using (HttpClient client = new HttpClient())
+    {
+      try
+      {
+        // Make the GET request
+        HttpResponseMessage response = await client.GetAsync(apiEndpoint);
+
+        // Check if the request was successful
+        response.EnsureSuccessStatusCode();
+
+        // Read and process the response content
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        // Output the response content (for demonstration purposes)
+        Console.WriteLine("API Response:");
+        Console.WriteLine(responseBody);
+      }
+      catch (HttpRequestException e)
+      {
+        // Handle any errors that occur during the request
+        Console.WriteLine($"Request error: {e.Message}");
+      }
+    }
+  }
+  */
+}
---
+++ ConsequencesTest/NSIStreamingProcessorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using USACE.HEC.Consequences;
+using USACE.HEC.Geography;
+using USACE.HEC.Hazards;
+using USACE.HEC.Results;
+
+namespace ConsequencesTest;
+public class NSIStreamingProcessorTest
+{
+  [Fact]
+  public void Test()
+  {
+    IBBoxStreamingProcessor sp = new NSIStreamingProcessor();
+    // int i = 0;
+    Location upperLeft = new Location { X = -40, Y = 50 };
+    IHazardProvider depthHazardProvider = new RandomDepthHazardProvider(25);
+    IResultsWriter consoleWriter = new ConsoleWriter();
+
+    sp.Process(depthHazardProvider.Extent(), (IConsequencesReceptor s) => {
+      Result r = s.Compute(depthHazardProvider.Hazard(upperLeft));
+      consoleWriter.Write(r);
+    });
+  }
+}
