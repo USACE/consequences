5cd6feac0724f8d6556a457db8d72cb89621887d
Jack <jackmschonherr@gmail.com>
2024-09-23 15:19:00 -0700

    initial native lib functionality

---
+++ AOTTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine(C.X());
+    Console.WriteLine(C.aotsample_add(1, 2));
+  }
+
+}
+
+public class C
+{
+  [DllImport("NativeLib.dll")]
+  public static extern int aotsample_add(int a, int b);
+  public static int X()
+  {
+    return 0;
+  }
+}
